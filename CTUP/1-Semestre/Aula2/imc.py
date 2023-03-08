fe5e66d5070ae1a85e5abd1bc01042c4905cd512
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 11:25:32 -0300

    elif & else

--- CTUP/1-Semestre/Aula2/imc.py
+++ CTUP/1-Semestre/Aula2/imc.py
@@ -1,3 +1,6 @@
+from xmlrpc.client import boolean
+
+
 peso = float(input("Quanto você pesa? "))
 altura = float(input("Qual a sua altura? "))
 
@@ -7,3 +10,8 @@ print("Seu imc é: " + str(imc))
 
 if imc > 30:
     print("reveja seus habitos")
+elif imc < 10:
+    print("você ta sumindo")
+else:
+    print("você ta tranquilo")
+
