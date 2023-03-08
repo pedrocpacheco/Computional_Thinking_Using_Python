b3e8de04c0ec155a38d9cc7f5b69031ab0401210
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 10:27:16 -0300

    printing, making ints and adding them to each other

--- CTUP/1-Semestre/Aula2/arquivo.py
+++ CTUP/1-Semestre/Aula2/arquivo.py
@@ -1,1 +1,16 @@
+# Escrever um texto
 print("Olá mundo")
+
+# Definir variavel e printa-la
+nome = "Leonardo"
+print(nome)
+
+# Definir ints e printar a soma
+a = 2
+b = 9
+print(a + b)
+
+# Se confundindo com ints
+a = "2"
+b = 9
+print(a + b)
