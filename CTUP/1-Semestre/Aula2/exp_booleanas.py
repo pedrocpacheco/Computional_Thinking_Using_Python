d85f5661110c0f86e7b6c4b8c37dca5c457e04de
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 11:54:30 -0300

    boolean conditions

---
+++ CTUP/1-Semestre/Aula2/exp_booleanas.py
@@ -0,0 +1,14 @@
+# Condições booleanas são condições que recebem valor TRUE ou FAlSE
+
+booleana = True # Nos podemos citar especificamente se o valor é true ou false
+booleana = 2 < 1 # Ou o valor pode resultar de uma condição booleana
+
+# Condicionais depende de condições booleanas para funcionarem
+num1 = 1
+num2 = 2
+
+if num1 < num2: # O resultado dessa condição é "true", pois num1 é menor que num2
+    print("Resultado true")
+
+if num1 > num2: # O resultado dessa condição é "false", pois num1 não é maior do que num2
+    print("Resultado falso")
