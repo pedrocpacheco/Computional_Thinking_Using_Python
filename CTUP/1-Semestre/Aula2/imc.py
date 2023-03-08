de4e0f31b170cfce88a3b21846e4b08711aa60b9
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 11:20:34 -0300

    if elif else

--- CTUP/1-Semestre/Aula2/imc.py
+++ CTUP/1-Semestre/Aula2/imc.py
@@ -2,5 +2,8 @@ peso = float(input("Quanto você pesa? "))
 altura = float(input("Qual a sua altura? "))
 
 
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 print("Seu imc é: " + str(imc))
+
+if imc > 30:
+    print("reveja seus habitos")
