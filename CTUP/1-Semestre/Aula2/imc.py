27fe4079a22f12ad3e3d9cd54a630ed3bdf5d07d
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 11:03:09 -0300

    using the same line for more things

--- CTUP/1-Semestre/Aula2/imc.py
+++ CTUP/1-Semestre/Aula2/imc.py
@@ -1,10 +1,6 @@
-peso = input("Quanto você pesa? ")
-altura = input("Qual a sua altura? ")
+peso = float(input("Quanto você pesa? "))
+altura = float(input("Qual a sua altura? "))
 
-peso = int(peso)
-altura = float(altura)
 
 imc = peso / (altura * altura)
-imc = str(imc)
-
-print("Seu imc é: " + imc)
+print("Seu imc é: " + str(imc))
