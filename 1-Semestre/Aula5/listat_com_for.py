45000cdd96330973d73958a57a6711a0b667d52b
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-17 11:48:06 -0300

    break e continue em loops

--- 1-Semestre/Aula5/listat_com_for.py
+++ 1-Semestre/Aula5/listat_com_for.py
@@ -3,3 +3,21 @@ games = ["Red Dead Redemption 2", "Cyberpunk 2077", "Tales From Borderlands"]
 
 for game in games:
   print ("Game " + game)
+
+# Break Com for
+for name in names:
+  if not name == "Daniel": 
+    print(name)
+  else:
+    break # o break para o laço de repetição
+
+# Continue com For
+numbers = [1, 2, 3, 4, 5, 6]
+for number in numbers:
+  print("Inicio do Laço")
+  if number % 2 == 0: # vendo se o numero é par
+    continue # o continue volta o laço desde o começo
+                
+  print(number) # só serão impressos os numeros impares
+                # Pois se o numero for par, ele parara no continue 
+                # o laço vai voltar pra cima
--- 1-Semestre/Aula5/litas_com_while.py
+++ 1-Semestre/Aula5/litas_com_while.py
@@ -2,3 +2,9 @@ number = 1
 while number <= 10:
     print("Chegando em 10, falta pouco! Já estamos em: " + str(number))
     number += 1  
+
+names = ["Pedro", "Kaua", "Daniel", "Souza", "Caio"]
+
+
+
+  
