0109303353b3c5c00d4091ffe032162cd7084342
pedrocpacheco <cpedropacheco@hotmail.com>
2023-03-22 10:37:46 -0300

    fazendo um resumo dos tipos de data

---
+++ 1-Semestre/Aula6/resumo.py
@@ -0,0 +1,14 @@
+texto = "esse é um texto" # String - texto
+num = 123 # int - numero
+ponto_flutuante = 1.5 # float - numero flutuante
+print(ponto_flutuante)
+
+
+# list = ["item 1", "index 1", 3, 3 [4.1, 4.2], 5]
+# print(list)
+
+lista = ["meu nome", 12, [1, 2, 3, 4]]
+print(lista)
+
+for item in lista[0]:
+    print("Item -> " + str(item))
