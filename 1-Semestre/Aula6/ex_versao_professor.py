26611e11fe92f960215970924e6f4ec4a58c5b8f
pedrocpacheco <cpedropacheco@hotmail.com>
2023-03-22 11:24:20 -0300

    esqueleto da resolucao que professor passou

---
+++ 1-Semestre/Aula6/ex_versao_professor.py
@@ -0,0 +1,17 @@
+nomes = []
+
+print("<Bem vindo ao programa>")
+print("Digite 1 -> Cadastrar Usuario")
+print("Digite 0 -> Sair do Programa")
+
+
+while True:
+    opcao = input("O que você deseja fazer? 1 ou 0? ")
+    if opcao == str(0):
+        break
+    else:
+        nome = input("Digite um nome para ser cadastrado: ")
+        nomes.append(nome)
+            
+
+print(nomes)
