9db78f3e38582c372f708f4e9130b47d878205a8
pedrocpacheco <cpedropacheco@hotmail.com>
2023-03-22 11:51:51 -0300

    fim da aula

--- 1-Semestre/Aula6/ex_versao_professor.py
+++ 1-Semestre/Aula6/ex_versao_professor.py
@@ -4,8 +4,6 @@ print("<Bem vindo ao programa>")
 print("Digite 0 -> Sair do Programa")
 print("Digite 1 -> Cadastrar Usuario")
 
-
-
 while True:
     opcao = input("O que você deseja fazer? 0 ou 1? ")
     if opcao == str(0):
