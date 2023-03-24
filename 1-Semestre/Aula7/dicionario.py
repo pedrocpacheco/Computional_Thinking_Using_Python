1b69690ccce0a70c4c0691e1308037953932107e
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-24 10:22:09 -0300

    iniciando o uso de funcionario

---
+++ 1-Semestre/Aula7/dicionario.py
@@ -0,0 +1,13 @@
+#Fazendo lista
+lista = []
+lista.append("item 1")
+lista.append("lista 2")
+
+#Fazendo dicionario
+dicionario = {
+  'first_name': 'Pedro',
+  'last_name': 'Pacheco',
+  'age': 17
+}
+
+print(dicionario)
