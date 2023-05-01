e7a782bc5ebd77ffd87c0afb4f4074e6ef96d203
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-04-30 21:30:37 -0300

    feat(CP2): finishing the chekpoint 2

--- 1-Semestre/CPs/CP-2/Loja/loja.py
+++ 1-Semestre/CPs/CP-2/Loja/loja.py
@@ -8,29 +8,35 @@ def define_product(id, name, description, value):
   return product
 
 def define_store(id, name):
+  store = {
+    "id": id,
+    "name": name
+  }
+
   product_list = []
   while(True):
-    option = input("What you want to do? \n1- Define Products | 2- Finish Product List\n")
+    option = input("What you want to do? \n1- Define Products | 2- Finish Product List \n")
     
     if(int(option) != 1):
       break
 
-    if(int(option) == 1):
-      product = define_product(int(input("Whats the Product ID? \n")), input("Whats the product name? \n"), input("Whats the product description? \n"), float(input("Whats the value of the product? \n")))
-      product_list.append(object)
+    product = define_product(int(input("Whats the Product ID? \n")), input("Whats the product name? \n"), input("Whats the product description? \n"), float(input("Whats the value of the product? \n")))
+    product_list.append(product)
 
-  store = {
-    "id": id,
-    "name": name,
-    "product_list": product_list
-  }
-  
+  store.update({
+        "id": id,
+        "name": name,
+        "product_list": product_list
+      }
+    )
+
   return store
+    
 
 def run_program():
   store_list = []
   while(True):
-    option_menu = int(input("What do you want to do? \n1- Define Stores | 2- Print The Store Info \n"))
+    option_menu = int(input("What do you want to do? \n1- Define Stores | 2- Print The Store Info | 3- Exit \n"))
     if(option_menu == 1):
       while(True):
         option_stores = int(input("What do you want to do? \n1- Define New Store | 2- Get back to menu \n"))
@@ -40,8 +46,10 @@ def run_program():
         name = input("Whats the new store name? \n")
         store = define_store(id, name) 
         store_list.append(store)
-    if(option_menu == 2):
-      print(store_list)    
+    elif(option_menu == 2):
+      print(store_list) 
+    else:
+      break   
 
 run_program()
       
