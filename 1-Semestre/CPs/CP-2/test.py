5ab0af96dd1837f50488aa8928c20250229b9411
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-01 15:08:15 -0300

    feat(CP2): deleting loja.py and adding my name

--- 1-Semestre/CPs/CP-2/test.py
+++ 1-Semestre/CPs/CP-2/test.py
@@ -1,3 +1,7 @@
 from Registration.functions import run_registration as run
 
 run()
+
+# Use este arquivo para dar run no programa, os outros so definem as funções.
+
+# Pedro Carvalho Pacheco | RM-98043
