abbe5de66d36f0a1340009737231a0666a8ee7ad
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-03 11:43:10 -0300

    feat: importing a json with requests module and treating it's txt

---
+++ 1-Semestre/Aula-5-03/aula.py
@@ -0,0 +1,22 @@
+import requests
+
+name = input("Digite seu nome: ").strip()
+name_json = requests.get(
+    f'http://servicodados.ibge.gov.br/api/v2/censos/nomes/{name}'
+).json()
+
+def print_name_info(name_json):
+    name = name_json[0]
+    print("\n<NAME INFO>\n")
+    print("(NORMAL INFOS)")
+    print(f"Name: ", name["nome"])
+    print(f"Sex: ", name["sexo"])
+    print(f"Location: ", name["localidade"])
+    print()
+    for res in name["res"]:
+        print("(RESIDENCIA) ")
+        print(f"Periodo Res: ", res["periodo"])
+        print(f"Frequência Res: ", res["frequencia"])
+        print()
+    
+print_name_info(name_json)
