a194be4155b347680b171d12bf85d51f830ce018
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-03 21:58:30 -0300

    test

--- 1-Semestre/Random/Aula3/funcoes.py
+++ 1-Semestre/Random/Aula3/funcoes.py
@@ -1,6 +1,6 @@
 # Declarar função usando def
 def mostrar_mensagem():
-    print("Mensagem")
+    print("Mensagem Teste para ver se mudar o nome do arquivo da problema")
 
 #Chamar função
 mostrar_mensagem()
