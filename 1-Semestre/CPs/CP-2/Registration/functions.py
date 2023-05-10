c7fdc78fc5341cb9db0edbd008454de5815086f5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-10 10:37:04 -0300

    refactor: CP2

--- 1-Semestre/CPs/CP-2/Registration/functions.py
+++ 1-Semestre/CPs/CP-2/Registration/functions.py
@@ -4,15 +4,18 @@ def run_registration():
   store_list = []
   while(True):
     option_menu = int(input("What do you want to do? \n1- Create Stores | 2- Print The Stores Info | 3- Exit \n"))
+    
     if(option_menu == 1):
       while(True):
         option_stores = int(input("What do you want to do? \n1- Define New Store | 2- Get back to menu \n"))
         if(option_stores == 2):
           break
+        
         id = int(input("Whats the new store id? \n"))
         name = input("Whats the new store name? \n")
         store = define_store(id, name) 
         store_list.append(store)
+    
     elif(option_menu == 2):
       print(f"<Stores>")
       print("-" * 15)
